5a7af0ba6cf3b2896a8595034730d56513e56fa7
HibbaHindawi <hibba04@hotmail.se>
2021-10-05 15:47:27 +0200

    wewew

--- .js
+++ .js
@@ -1,8 +1,10 @@
-var music = new Audio('lessgo.mp3')
+var music = new Audio('lessgoo.mp3')
 var pressed = false;
 function button(){ //this changes a button to do two different things
     if(!pressed){
+        music.currentTime = 0;
         music.play();
+        music.loop = true;
         document.getElementById('myImage').src='dababy.gif'; document.getElementById('myImage').style.width = '35%'
         document.getElementById('myImage').style.height = '35%'
         document.querySelector('.lessgo').style.visibility = 'visible'
@@ -12,6 +14,7 @@ function button(){ //this changes a button to do two different things
     }
     else{
         music.pause();
+        music.loop = false;
         document.getElementById('myImage').src='https://marketplacer.imgix.net/vJ/Tx58gaXWG74TcY55mqbkeuCmQ.jpg?auto=format&fm=pjpg&fit=max&q=90&itemprop=image&alt=Top%20tips%20to%20help%20your%20baby%27s%20development&w=1600&h=1000&s=290f80053944f9ec023ead946a59f60a'
         document.querySelector('.lessgo').style.visibility = 'hidden'
         document.querySelector('.lessgo2').style.visibility = 'hidden'
